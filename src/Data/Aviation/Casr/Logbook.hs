557395deab83e930462f996014ff304a094315d0
Tony Morris <tmorris@tmorris.net>
2016-08-24 19:41:23 +1000

    Moved to casr-logbook-types and join other casr-logbook-* packages

--- src/Data/Aviation/Casr/Logbook.hs
+++ src/Data/Aviation/Casr/Logbook.hs
@@ -2,21 +2,11 @@ module Data.Aviation.Casr.Logbook (
   module L
 ) where
 
-import Data.Aviation.Casr.Logbook.Logbook as L
-import Data.Aviation.Casr.Logbook.Rating as L
-import Data.Aviation.Casr.Logbook.Aviator as L
-import Data.Aviation.Casr.Logbook.Engine as L
-import Data.Aviation.Casr.Logbook.Aircraft as L
-import Data.Aviation.Casr.Logbook.TimeAmount as L
-import Data.Aviation.Casr.Logbook.DayNight as L
-import Data.Aviation.Casr.Logbook.Location as L
-import Data.Aviation.Casr.Logbook.Command as L
-import Data.Aviation.Casr.Logbook.Time as L
-import Data.Aviation.Casr.Logbook.FlightPoint as L
-import Data.Aviation.Casr.Logbook.FlightPath as L
-import Data.Aviation.Casr.Logbook.AircraftFlight as L
-import Data.Aviation.Casr.Logbook.SimulatorFlight as L
-import Data.Aviation.Casr.Logbook.Exam as L
-import Data.Aviation.Casr.Logbook.Briefing as L
-import Data.Aviation.Casr.Logbook.Entry as L
-import Data.Aviation.Casr.Logbook.Entries as L
+import Data.Aviation.Casr.Logbook.Types as L
+import Data.Aviation.Casr.Logbook.Meta as L
+import Data.Aviation.Casr.Logbook.Html.Html as L
+import Data.Aviation.Casr.Logbook.Meta.Html as L
+import Data.Aviation.Casr.Logbook.Reports as L
+import Data.Aviation.Casr.Logbook.Reports.Html as L
+import Data.Aviation.Casr.Logbook.Reports.Meta as L
+import Data.Aviation.Casr.Logbook.Reports.Meta.Html as L
