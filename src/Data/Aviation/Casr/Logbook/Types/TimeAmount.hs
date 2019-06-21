5ea1e3ffdfcd0751cbda56c0c38246c19ce74548
Tony Morris <tmorris@tmorris.net>
2019-06-22 00:35:32 +1000

    update digit dependency

--- src/Data/Aviation/Casr/Logbook/Types/TimeAmount.hs
+++ src/Data/Aviation/Casr/Logbook/Types/TimeAmount.hs
@@ -10,14 +10,10 @@ module Data.Aviation.Casr.Logbook.Types.TimeAmount(
 , timeAmountBy10
 ) where
 
-import Data.Bool(Bool, bool)
-import Data.Maybe(fromMaybe)
-import Data.Ord((>))
 import Data.Semigroup(Semigroup((<>)))
-import Prelude(Integral, divMod, mod)
-import Control.Lens(makeClassy, (^?), over, _1, ( # ))
+import Control.Lens(makeClassy, ( # ))
 import Data.Eq(Eq)
-import Data.Digit(DecDigit, x0, x1, integralDecimal)
+import Data.Digit(DecDigit, x0, integralDecimal, addDecDigit')
 import Data.Int(Int)
 import Data.Monoid(Monoid(mempty, mappend))
 import Data.Ord(Ord)
@@ -47,7 +43,7 @@ addtimeamount ::
   -> TimeAmount
   -> TimeAmount
 TimeAmount f1 p1 `addtimeamount` TimeAmount f2 p2 =
-  let (h, q) = p1 /+/ p2
+  let (h, q) = p1 `addDecDigit'` p2
   in  TimeAmount (f1 + f2 + integralDecimal # (h :: DecDigit)) q 
 
 timeAmountBy10 ::
@@ -65,29 +61,3 @@ instance Monoid TimeAmount where
     zerotimeamount
   mappend =
     (<>)
-
----- belongs in digit package
-
-mod10 ::
-  Integral a =>
-  a
-  -> DecDigit
-mod10 n =
-  let r = n `mod` 10
-  in fromMaybe (mod10 r) (r ^? integralDecimal)
-
-addDecDigit ::
-  DecDigit
-  -> DecDigit
-  -> (Bool, DecDigit)
-addDecDigit a b =
-  let (x, r) =
-        (integralDecimal # a + integralDecimal # b) `divMod` 10
-  in  (x > 0, mod10 (r :: Int))
-
-(/+/) ::
-  DecDigit
-  -> DecDigit
-  -> (DecDigit, DecDigit)
-(/+/) a b =
-  over _1 (bool x0 x1) (addDecDigit a b)
